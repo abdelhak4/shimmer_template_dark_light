21b756c600f0f27adc1ea6a2820ccd1fad58e92b
Abdelhak El <teqaniabde@gmail.com>
2024-11-29 20:53:33 +0100

    init

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:fading_animation/config/app_theme.dart';
+import 'package:fading_animation/views/main_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      home: const FinalView() ,
+    );
+  }
+}
